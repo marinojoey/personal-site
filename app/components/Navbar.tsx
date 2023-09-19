25ef05b7361c3e2030265e28cdc0bb502c06401c
Joey Marino <marinojoey@pm.me>
2023-09-19 12:38:03 -0600

    Add Koulen font to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link'
 import React, { FC } from 'react'
+import { Koulen } from 'next/font/google'
 
 interface NavBarProps {
   // TODO: define props here
 }
 
+const koulen = Koulen({ subsets: ['khmer'], style: 'normal', weight: '400' })
+
 const NavBar: FC<NavBarProps> = () => {
   return (
-    <nav className="navbar">
+    <nav className={`${koulen.className} navbar`}>
       <div className="linkGroup">
         <Link href="/resume" className="navLink">
           Resume
